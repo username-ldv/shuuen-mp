d49ae6a4db6d07f3a6ad20d174152d7d782bb1b0
username-ldv <125184953+username-ldv@users.noreply.github.com>
2026-06-08 11:58:26 +0300

    starting desktop maximized

--- desktopApp/src/main/kotlin/ldv/shuuen/main.kt
+++ desktopApp/src/main/kotlin/ldv/shuuen/main.kt
@@ -1,10 +1,16 @@
 package ldv.shuuen
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
+import kotlinx.coroutines.delay
 import ldv.shuuen.logging.Utf8StdoutHandler
+import kotlin.time.Duration.Companion.milliseconds
 
 fun main() {
   Napier.base(DebugAntilog(handler = listOf(Utf8StdoutHandler())))
@@ -13,7 +19,13 @@ fun main() {
     Window(
       onCloseRequest = ::exitApplication,
       title = "Shuuen",
+      state = rememberWindowState(width = 1200.dp, height = 800.dp, placement = WindowPlacement.Maximized),
     ) {
+      LaunchedEffect(Unit) {
+        window.isAlwaysOnTop = true
+        delay(200.milliseconds)
+        window.isAlwaysOnTop = false
+      }
       App()
     }
   }
